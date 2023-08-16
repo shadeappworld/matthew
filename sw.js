fb6a34d8b4309a6eeb0a0cd80820471f0fa8a4e9
Shade Software <121112889+shadeappworld@users.noreply.github.com>
2023-08-16 20:36:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -52,3 +52,20 @@ self.addEventListener('fetch', (event) => {
     })());
   }
 });
+
+const CACHE_NAME = 'cool-cache';
+
+// Add whichever assets you want to pre-cache here:
+const PRECACHE_ASSETS = [
+    '/index.html',
+    '/script.js'
+]
+
+// Listener for the install event - pre-caches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
+});
+
